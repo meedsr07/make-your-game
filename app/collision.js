6ae3e68ae3177b87b9830ea777dbdb0557fa90e6
msarar <mohammedsarar7@gmail.com>
2026-06-22 14:53:46 +0100

    fixing

--- app/collision.js
+++ app/collision.js
@@ -2,61 +2,63 @@ import { gamePlay as G } from "./state.js";
 
 
 export function checkBulletEnemyCollision() {
-		if (!G.bullet) return 	
-		let bullet = G.bullet 
-		if (hitShield(bullet)  ) {
-            return 
-        }
-        // get the position of the bullet part of the screen
-        // loop  in  enemies in the G.spawnedMobs array  
-        for (let r = 0; r < G.spawnedMobs.length; r++) {
-            let row = G.spawnedMobs[r][0];
-			
 
-            for (let m = 0; m < row.length; m++) {
-				
-                let enemy = row[m];
-                if (!enemy.alive) continue;
-                
-                // check if the bullet rectangle intersects with the enemy rectangle
-				 const hit = 
-			    G.bullet.x < enemy.x + enemy.width  &&
-   				G.bullet.x + G.bullet.width > enemy.x  &&
-			    G.bullet.y < enemy.y + enemy.height &&
-			    G.bullet.y + G.bullet.height > enemy.y;
-                    if (hit) {
+    if (!G.bullet) return
+    let bullet = G.bullet
+    // if the bullet collided with a shield
+    if (hitShield(bullet)) {
+        return
+    }
+    // looping for the enemy arr
+    for (let r = 0; r < G.spawnedMobs.length; r++) {
+        let row = G.spawnedMobs[r][0];
 
-                    bullet.element.remove();
+        for (let m = 0; m < row.length; m++) {
 
-                    enemy.kill();
-					G.aliveMobs--
-                    G.bullet = null;
-              
+            let enemy = row[m];
+            // skip the enemy if he dead 
+            if (!enemy.alive) continue;
 
-                    return
-                }
+            // check if the bullet rectangle intersects with the enemy rectangle
+            const hit =
+                G.bullet.x < enemy.x + enemy.width &&
+                G.bullet.x + G.bullet.width > enemy.x &&
+                G.bullet.y < enemy.y + enemy.height &&
+                G.bullet.y + G.bullet.height > enemy.y;
+
+            if (hit) {
+
+                bullet.element.remove();
+
+                enemy.kill();
+                G.aliveMobs--
+                G.bullet = null;
+
+
+                return
             }
         }
-	if (G.ufo) {
-		let enemy = G.ufo 
-					 const hit = 
-			    G.bullet.x < enemy.x + enemy.width  &&
-   				G.bullet.x + G.bullet.width > enemy.x  &&
-			    G.bullet.y < enemy.y + enemy.height &&
-			    G.bullet.y + G.bullet.height > enemy.y;
-                    if (hit) {
+    }
+    if (G.ufo) {
+        let enemy = G.ufo
+        const hit =
+            G.bullet.x < enemy.x + enemy.width &&
+            G.bullet.x + G.bullet.width > enemy.x &&
+            G.bullet.y < enemy.y + enemy.height &&
+            G.bullet.y + G.bullet.height > enemy.y;
+        if (hit) {
 
-                    bullet.element.remove();
+            bullet.element.remove();
 
-                    enemy.kill();
-					G.aliveMobs--
-			G.ufo = null
-                    G.bullet = null;
-			   }
+            enemy.kill();
+            G.aliveMobs--
+            G.ufo = null
+            G.bullet = null;
+        }
 
-	}
-	
-	
+    }
+
+
 }
 
 
@@ -64,14 +66,14 @@ export function checkBulletEnemyCollision() {
 
 function hitShield(b) {
 
-    for (let py = b.y; py <= b.y + b.height/2; py++) {
-        for (let px = b.x; px <= b.x + b.width/2; px++) {
+    for (let py = b.y; py <= b.y + b.height / 2; py++) {
+        for (let px = b.x; px <= b.x + b.width / 2; px++) {
             const key = `${px},${py}`;
             if (G.bricks.has(key)) {
                 G.bricks.get(key).element.remove();
                 G.bricks.delete(key);
                 b.element.remove();
-	            G.bullet = null;
+                G.bullet = null;
                 return true;
             }
         }
